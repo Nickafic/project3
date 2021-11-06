ff3aaee4a2ac04d6b6af6ea6e79756e341250b35
Nickafic <85850258+Nickafic@users.noreply.github.com>
2021-11-06 11:52:17 -0700

    tree 2

--- DriverBT.java
+++ DriverBT.java
@@ -77,6 +77,29 @@ public class DriverBT
 	/**Task 4: Create a tree case as shown in assignment 3*/
 	public static void createTree2(BinaryTree<String> tree)
 	{ 
+		BinaryTree<String> bTree = new BinaryTree<>("B");
+		BinaryTree<String> fTree = new BinaryTree<>("F");
+		BinaryTree<String> hTree = new BinaryTree<>("H");
+
+		BinaryTree<String> gTree = new BinaryTree<>("G", null, hTree);
+		BinaryTree<String> eTree = new BinaryTree<>("E", null, gTree);
+		BinaryTree<String> dTree = new BinaryTree<>("D", fTree, null);
+		BinaryTree<String> cTree = new BinaryTree<>("C", dTree, eTree);
+
+		tree.setTree("A", bTree, cTree);
+
+		System.out.println("\nGiven Tree:\n");
+      System.out.println("     A      ");
+      System.out.println("   /   \\  ");
+      System.out.println("  B     C  ");
+      System.out.println("       / \\  ");
+      System.out.println("      D    E   ");
+      System.out.println("     /      \\");
+      System.out.println("    F        G ");
+	  System.out.println("              \\");
+	  System.out.println("               H "); 
+      System.out.println();
+
 
 	} // end createTree2
 
